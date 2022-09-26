c2d59315e136876303b531ce3c012b39ed17d288
mykyta stoliarov <mykyta.stoliarov@gmail.com>
2022-09-26 16:45:14 -0400

    scoop practice

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    const output = `${firstName} you are ${age}, born in ${birthYear}`;
+    console.log(output);
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      let str = `you are millenial ${age}`;
+      console.log(str);
+    }
+    console.log(millenial);
+  }
+  printAge();
+  return age;
+}
+const firstName = 'Mykyta';
+calcAge(1997);
