2aab6708f382f855f0b39b73bbe93273e464e409
mykyta stoliarov <mykyta.stoliarov@gmail.com>
2022-10-11 13:25:19 -0400

    started a project Bankist

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -89,35 +89,46 @@
 
 //  CODING CHALLENGE #1
 
-const poll = {
-  question: 'What is your favourite programming language?',
-  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
-  answers: new Array(4).fill(0),
+// const poll = {
+//   question: 'What is your favourite programming language?',
+//   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+//   answers: new Array(4).fill(0),
 
-  registerNewAnswer: function () {
-    let answer = Number(
-      prompt(
-        `${this.question}\n${poll.options.join('\n')}\n(Write option number)\n`
-      )
-    );
-    typeof answer === 'number' &&
-      answer < this.answers.length &&
-      this.answers[answer]++;
+//   registerNewAnswer: function () {
+//     let answer = Number(
+//       prompt(
+//         `${this.question}\n${poll.options.join('\n')}\n(Write option number)\n`
+//       )
+//     );
+//     typeof answer === 'number' &&
+//       answer < this.answers.length &&
+//       this.answers[answer]++;
 
-    this.displayResults();
-    this.displayResults('string');
-  },
-  displayResults(type = 'array') {
-    if (type === 'array') {
-      console.log(this.answers);
-    } else if (type === 'string') {
-      console.log(`Poll results are ${this.answers.join(', ')}`);
-    }
-  },
+//     this.displayResults();
+//     this.displayResults('string');
+//   },
+//   displayResults(type = 'array') {
+//     if (type === 'array') {
+//       console.log(this.answers);
+//     } else if (type === 'string') {
+//       console.log(`Poll results are ${this.answers.join(', ')}`);
+//     }
+//   },
+// };
+
+// //poll.registerNewAnswer();
+// let answerPoll = document.querySelector('.poll');
+// answerPoll.addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+// poll.displayResults.call({ answers: [5, 2, 3] });
+
+const secureBooking = function () {
+  let passengerCount = 0;
+  return function () {
+    passengerCount++;
+    console.log(passengerCount);
+  };
 };
 
-//poll.registerNewAnswer();
-let answerPoll = document.querySelector('.poll');
-answerPoll.addEventListener('click', poll.registerNewAnswer.bind(poll));
+const booker = secureBooking();
 
-poll.displayResults.call({ answers: [5, 2, 3] });
--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,16 +61,51 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  movements.forEach((value, index, arr) => {
+    const condition = value < 0 ? 'withdrawal' : 'deposit';
+    const html = `  
+    <div class="movements__row">
+     <div class="movements__type movements__type--${condition}">${condition}</div>
+     <div class="movements__value">${value}€</div>
+   </div>
+   `;
+    //containerMovements.innerHTML += html;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+displayMovements(account1.movements);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+// for (const movement of movements) {
+//   if (movement > 0) {
+//     console.log(`You deposited ${movement}$`);
+//   } else {
+//     console.log(`You withdraw ${Math.abs(movement)}$`);
+//   }
+// }
+const dogsJulia = [3, 5, 2, 12, 7];
+const dogsKate = [4, 1, 15, 8, 3];
+function checkDogs(dogsJulia, dogsKate) {
+  let shallowCopyJulia = dogsJulia.slice(1, -2);
+  console.log(shallowCopyJulia);
+  let dogArr = shallowCopyJulia.concat(dogsKate);
+  console.log(dogArr);
+  dogArr.forEach((value, index, arr) => {
+    value > 3 ? console.log('adult') : console.log('puppy');
+  });
+}
+checkDogs(dogsJulia, dogsKate);
